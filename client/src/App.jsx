9c429f9a938e075dd788d3354fcf4e8f9f882c84
Jorge Rojas <exxscher@gmail.com>
2022-05-10 17:45:50 -0400

    Basic dark/Light mode added, without localstorage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,28 @@
 import './App.css';
-import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import useLocalStorage from 'use-local-storage'
 import Leer from './views/Leer';
 import Contar from './views/Contar';
 import Administrar from './views/Administrar';
 import NotFound from './components/NotFound';
 import SecretDetail from './components/SecretDetail';
 
+document.body.setAttribute("data-theme", "dark");
+
+const switchTheme = () => {
+  document.body.getAttribute("data-theme") === "dark" ?
+    document.body.setAttribute("data-theme", "light") :
+    document.body.setAttribute("data-theme", "dark");
+}
+
 function App() {
-
-  // const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  // const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light');
-
-  // const switchTheme = () => {
-  //   const newTheme = theme === "light" ? "dark" : "light"
-  //   setTheme(newTheme);
-  //   document.body.setAttribute("data-theme", theme)
-  // }
-
-  // useEffect(() => {
-  //   document.body.setAttribute("data-theme", theme)
-  // }, []);
-
   return (
     <div className="container">
+      <div className="dark-light">
+        <button onClick={() => switchTheme()}><i className="fas fa-adjust"></i></button>
+      </div>
       <header>
         <h1><i className="fa-solid fa-eye-low-vision"></i><span className='logo-bold'>mi</span>secreto</h1>
-        <div className="dark-light">
-          <button onClick={() => document.body.setAttribute("data-theme", "dark")}>Dark</button>
-          <span>/</span>
-          <button onClick={() => document.body.setAttribute("data-theme", "light")}>Light</button>
-        </div>
       </header>
-      {/* <button onClick={switchTheme}>Switch to {theme === "light" ? "Light" : "Dark"} Theme</button> */}
       <BrowserRouter>
         <nav>
           <Link to="/">Leer</Link>
